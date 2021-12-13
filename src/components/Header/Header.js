cb7698d0581a207318e2a616ad9af6b9ab96f1b8
sunday odum <sundayodum@gmail.com>
2021-12-13 22:04:58 +0100

    project.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -32,10 +32,10 @@ const Header = () =>  (
       </li>        
     </Div2>
       <Div3>
-        <SocialIcons href="https://google.com">
+        <SocialIcons href="https://github.com/sunday-odum">
           <AiFillGithub size="3rem" />
         </SocialIcons>
-        <SocialIcons href="https://google.com">
+        <SocialIcons href="https://www.linkedin.com/in/odum-sunday-928727ba">
           <AiFillLinkedin size="3rem" />
         </SocialIcons>
         <SocialIcons href="https://google.com">
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,20 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome To <br />
+          My Personal Portfolio
+        </SectionTitle>
+        <SectionText>
+        Developing easy and fun to use awesome applications.
+        </SectionText>
+        <Button onClick={props.handleClick}>Learn More</Button>
+      </LeftSection>
+    </Section>
+  </>
 );
 
 export default Hero;
